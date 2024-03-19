838f2d62b447ebfe772a75ed2e581d927586c530
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-03-19 11:43:47 -0600

    PHA profile Info CSS acceptable, now functionality of importing data

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, lowercase: true, unique: true },
+    username: { type: String, required: true, unique: true },
     // firstName: { type: String, required: true },
     // lastName: {type: String, required: true },
     email: { type: String, required: true, lowercase: true, unique: true },
--- src/pages/PHAProfilePage/PHAProfilePage.jsx
+++ src/pages/PHAProfilePage/PHAProfilePage.jsx
@@ -1,23 +1,57 @@
-export default function PHAProfilePage(){
+import { Link } from "react-router-dom";
+import "./PHAProfilePage.css"
+import React from 'react'
+
+
+export default function PHAProfilePage() {
     return (
         <>
-        <h1>THIS IS THE PHA PROFILE PAGE</h1>
-        <ul>TO DOS:
-            <li>
-                name 
-            </li>
-            <li>
-                email
-            </li>
-            <li>
-                role: PHA
-            </li>
-            <li>EDIT button</li>
-            <li>DELETE BUTTON</li>
-            <li>CREATE NEW PROPERTY BUTTON</li>
-            <li>VIEW MY PROPERTIES BUTTON</li>
+            <div className="yellow_top_page">
+                <img className="hqspec_logo" src="https://i.imgur.com/CKEt2Kqm.png" alt="" />
+                <header className="title">
+                    <br />
+                    <h1 style={{
+                        fontSize: "10vh"
+                    }}>PHA PROFILE</h1>
+                    <h2 style={{
+                        fontsize: "10vh"
+                    }}></h2>
+                </header>
+                <section className="PHA_Profile_Info">
+                    <form className="Profile_Container">
+                        <div className="PHA_User_Name_Title">
+                            Name:
+                        </div>
+                        <div className="PHA_User_Name_Input">
+                            USER NAME!
+                        </div>
+                        <div className="PHA_User_Email_Title">
+                            Email:
+                        </div>
+                        <div className="PHA_User_Email_Input">
+                            blah@mail.com
+                        </div>
+                        <div className="PHA_User_Role_Title">
+                            User Role:
+                        </div>
+                        <div className="PHA_User_Role_Input">
+                            PHA
+                        </div>
+                    </form>
+            </section >
 
-        </ul>
+            <ul>TO DOS:
+
+                <li>
+                    role: PHA
+                </li>
+                <li>EDIT button</li>
+                <li>DELETE BUTTON</li>
+                <li>CREATE NEW PROPERTY BUTTON</li>
+                <li>VIEW MY PROPERTIES BUTTON</li>
+
+            </ul>
+        </div >
         </>
     )
 }
