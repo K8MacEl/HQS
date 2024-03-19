4d1e01946fed8225c9d86edc6ab8e6c9020d1a8b
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-03-18 19:13:34 -0600

    login and sign up buttons aligned

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -7,29 +7,38 @@ import "./IndexPage.css";
 export default function IndexPage() {
     return (
         <>
-            <div className="yellow_top_page" >
+            <div className="yellow_top_page">
                 <img className="hqspec_logo" src="https://i.imgur.com/CKEt2Kqm.png" alt="" />
                 <header className="title">
-                    <br></br>
+                    <br />
                     <h1>HQS Inspection Tool</h1>
                     <h3>Designed to Assist with HUD's Inspection Form for the Housing Choice Voucher Program</h3>
-                    <br></br>
+                    <br />
                 </header>
-                <nav>
-                    <Link to="/login" className="login_button" >
-                        <button style={{height: "8vh", width: "15vh", background: "white", margin: "10px", borderRadius: "10px"}}> LOGIN </button>
-                    </Link>
-                    <Link to="/signup" className="signup_button">
-                        <button style={{height: "8vh", width: "15vh", background: "white", borderRadius: "10px"}}>SIGN UP</button>
-                    </Link>
+                <nav className="buttons">
+                    <div className="login_button">
+                        <Link to="/login">
+                            <button style={{ height: "8vh", width: "15vh", background: "white", margin: "10px", borderRadius: "10px" }}> LOGIN </button>
+                        </Link>
+                    </div>
+                    <div className="signup_button">
+                        <Link to="/signup">
+                            <button style={{ height: "8vh", width: "15vh", background: "white", borderRadius: "10px" }}>SIGN UP</button>
+                        </Link>
+                    </div>
                 </nav>
+                <div className="section-container">
+                    <div className="section">
+                        <p className="textblock_one">HQS Inspectors Create a Profile that will track all of their inspections.</p>
+                    </div>
+                    <div className="section">
+                        <p className="textblock_two">HQSpec guides inspectors through the Inspection form and allows for photo uploads.</p>
+                    </div>
+                    <div className="section">
+                        <p className="textblock_three">HQSpec populates the HUD Inspection Form PDF for the inspector to submit.</p>
+                    </div>
+                </div>
             </div>
         </>
-    )
+    );
 }
-
-import React from 'react'
-
-
-
-
