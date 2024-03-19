93038a8298371488602e19ed5e326002ccb10267
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-03-18 19:27:07 -0600

    homepage completed

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -11,19 +11,36 @@ export default function IndexPage() {
                 <img className="hqspec_logo" src="https://i.imgur.com/CKEt2Kqm.png" alt="" />
                 <header className="title">
                     <br />
-                    <h1>HQS Inspection Tool</h1>
-                    <h3>Designed to Assist with HUD's Inspection Form for the Housing Choice Voucher Program</h3>
+                    <h1 style={{
+                        fontSize: "10vh"
+                    }}>HQS Inspection Tool</h1>
+                    <h2 style={{
+                        fontsize: "10vh"
+                    }}>Designed to Assist with HUD's Inspection Form for the Housing Choice Voucher Program</h2>
                     <br />
                 </header>
                 <nav className="buttons">
                     <div className="login_button">
                         <Link to="/login">
-                            <button style={{ height: "8vh", width: "15vh", background: "white", margin: "10px", borderRadius: "10px" }}> LOGIN </button>
+                            <button style={{ 
+                                height: "8vh", 
+                                width: "15vh", 
+                                background: "white",
+                                margin: "10px", 
+                                borderRadius: "10px" }}> 
+                                LOGIN 
+                            </button>
                         </Link>
                     </div>
                     <div className="signup_button">
                         <Link to="/signup">
-                            <button style={{ height: "8vh", width: "15vh", background: "white", borderRadius: "10px" }}>SIGN UP</button>
+                            <button style={{ 
+                                height: "8vh", 
+                                width: "15vh", 
+                                background: "white", 
+                                borderRadius: "10px" }}>
+                                SIGN UP
+                            </button>
                         </Link>
                     </div>
                 </nav>
