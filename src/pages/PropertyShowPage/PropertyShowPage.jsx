be0b1daacb0f8a332c6c2847bc2e84b432d98a6f
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-03-21 15:49:00 -0600

    property information show page css

--- src/pages/PropertyShowPage/PropertyShowPage.jsx
+++ src/pages/PropertyShowPage/PropertyShowPage.jsx
@@ -13,7 +13,7 @@ export default function PropertyShowPage() {
     async function getProperty() {
         try {
             //user params to grab that id
-            
+
             const response = await fetch(`/api/properties/${propertyId}`, {
                 method: "GET",
                 headers: {
@@ -23,7 +23,7 @@ export default function PropertyShowPage() {
             const data = await response.json();
             setLoading(false)
             console.log(data, "this is data");
-           setProperty(data.propertyDoc)
+            setProperty(data.propertyDoc)
         } catch (err) {
             console.log(err);
         }
@@ -36,56 +36,43 @@ export default function PropertyShowPage() {
     console.log("this is property-->", property)
 
     return (
-
         <>
             <div className="property_body">
                 <img className="hqspec_logo" src="https://i.imgur.com/CKEt2Kqm.png" alt="" />
                 <header className="title">
-                    <br />
-                    <h1 style={{
-                        fontSize: "10vh"
-                    }}>PROPERTY INFORMATION</h1>
-                    <h2 style={{
-                        fontsize: "10vh"
-                    }}></h2>
+                    <h1 style={{ fontSize: "10vh" }}>PROPERTY INFORMATION</h1>
+                    <h2 style={{ fontSize: "10vh" }}></h2> {/* Fixed fontSize */}
                 </header>
+                    <div className="address_title"><strong>Property Address:</strong></div>
+                <nav className="address">
+                    <div className="street_address">{property.streetNumber} {property.streetName}, {property.unit}</div>
+                    <div>{property.city}, {property.state} {property.zip}</div>
+                </nav>
                 <section className="property_information">
-                        <div className="property_address_title">
-                            Property Address:
-                        </div>
-                        <div className="property_address_input">
-                            { property.streetNumber }
-                            { property.streetName }
-                            { property.unit }
-                            { property.city }
-                            { property.state }
-                            { property. zip }
-
-                        </div>
                     <div className="property_box">
                         <div className="voucher_holder_title">
                             Voucher Holder:
                         </div>
                         <div className="voucher_holder_input">
-                            { property.voucherHolder }
+                            {property.voucherHolder}
                         </div>
                         <div className="tenant_id_title">
                             Tenant ID Number:
                         </div>
                         <div className="tenant_id_input">
-                            { property.tenantIdNumber}
+                            {property.tenantIdNumber}
                         </div>
                         <div className="property_owner_title">
-                            Property Owner:  
+                            Property Owner:
                         </div>
                         <div className="property_owner_input">
-                             { property.propertyOwner }
+                            {property.propertyOwner}
                         </div>
                         <div className="census_title">
                             Census Track:
                         </div>
                         <div className="census_input">
-                            { property.censusTrack}
+                            {property.censusTrack}
                         </div>
                         <form className="request_inspection">
                             <h3>INSPECTION REQUEST OPTION HERE</h3>
@@ -112,6 +99,7 @@ export default function PropertyShowPage() {
             <section className="inspections_embedded">
                 INSPECTIONS EMBEDDED HERE!
             </section>
-        </>
+            </>
     )
 } 
+
